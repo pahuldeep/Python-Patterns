5f4e0945737068964c6fd52cf8b2aa180be8165c
pahuldeep <pahuldeep100@gmail.com>
2025-09-25 02:12:31 +0530

    update for patterns only

---
+++ Object-Oriented/inheritance.py
@@ -0,0 +1,12 @@
+from typing import List, Optional
+
+class Contact:
+    all_contacts: List["Contact"] = []
+
+    def __init__(self, name: str, email: str) -> None:
+        self.name = name
+        self.email = email
+        Contact.all_contacts.append(self)
+
+    def __repr__(self) -> str:
+        return
